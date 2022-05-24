4e8c6001299e5814d17168e92691e9ec4eb4fc88
ArnabShikder24 <arnabshikder2001@gmail.com>
2022-05-24 19:17:53 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, Db } = require('mongodb');
+const port = process.env.PORT || 5000;
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lk1qu.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const productCollection = client.db('bicycle_zen').collection('products');
+    }
+    finally {}
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('Bicyclezen is Running On Server...')
+});
+
+app.listen(port, () => console.log('Bicyclezen running this port: ', port));
